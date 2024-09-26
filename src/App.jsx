6d3cb74e4b6366b429bce886b797045774837410
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2024-09-26 15:16:12 +0100

    lazy load inplemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Home from "./pages/Home";
-import Shops from "./pages/Shops";
-import Cart from "./pages/Cart";
-import Delivery from "./pages/Delivery";
-import Details from "./pages/Details";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import CategoryShop from "./pages/CategoryShop";
 import { get_categories } from "./store/reducers/homeReducer";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import SearchProducts from "./pages/SearchProducts";
+import { lazy, useEffect } from "react";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import Home from "./pages/Home";
+// import Shops from "./pages/Shops";
+// import Cart from "./pages/Cart";
+// import Delivery from "./pages/Delivery";
+// import Details from "./pages/Details";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
+// import CategoryShop from "./pages/CategoryShop";
+// import Payment from "./pages/Payment";
+// import SearchProducts from "./pages/SearchProducts";
+
+
 import AppLayout from "./components/layouts/AppLayout";
-import Payment from "./pages/Payment";
 import ProtectUser from "./utils/ProtectUser";
-import Dashboard from "./pages/Dashboard";
-import Index from "./components/dashboard";
-import Orders from "./components/dashboard/Orders";
+const Home = lazy(() => import("./pages/Home"));
+const Shops = lazy(() => import("./pages/Shops"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Delivery = lazy(() => import("./pages/Delivery"));
+const Details = lazy(() => import("./pages/Details"));
+const Register = lazy(() => import("./pages/Register"));
+const Login = lazy(() => import("./pages/Login"));
+const CategoryShop = lazy(() => import("./pages/CategoryShop"));
+const SearchProducts = lazy(() => import("./pages/SearchProducts"));
+const Payment = lazy(() => import("./pages/Payment"));
+const Index = lazy(() => import("./components/dashboard"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Orders = lazy(() => import("./components/dashboard/Orders"));
 
 const router = createBrowserRouter([
   {
