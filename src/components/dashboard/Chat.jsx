fcb07b616eeadb472c5762d19416e905958611e0
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2024-10-10 16:59:05 +0100

    customer to seller chat feature ongoing

--- src/components/dashboard/Chat.jsx
+++ src/components/dashboard/Chat.jsx
@@ -1,12 +1,40 @@
+import { useEffect } from "react";
 import { AiOutlineMessage, AiOutlinePlus } from "react-icons/ai";
 import { GrEmoji } from "react-icons/gr";
 import { IoSend } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import io from 'socket.io-client'
+import { add_friend } from "../../store/reducers/chatReducer";
+
+const socket = io("http://localhost:5000");
+
 
 function Chat() {
+const dispatch = useDispatch();
+
+  
+  const { sellerId } = useParams();
+  const { userInfo } = useSelector((state) => state.auth);
+  
+
+
+   useEffect(() => {
+     socket.emit("add_user", userInfo.id, userInfo);
+   }, [userInfo]);
+
+   useEffect(() => {
+     dispatch(
+       add_friend({
+         sellerId: sellerId || "",
+         userId: userInfo.id,
+       })
+     );
+   }, [dispatch, sellerId, userInfo]);
+
   return (
     <div className="bg-white dark:bg-[#232D3F]  p-3 rounded-md">
-      <div className="w-full flex">
+      <div className="flex w-full">
         <div className="w-[230px]">
           <div className="flex justify-center gap-3 items-center text-slate-600 dark:text-white text-xl h-[50px]">
             <span>
@@ -14,10 +42,10 @@ function Chat() {
             </span>
             <span>Message</span>
           </div>
-          <div className="w-full flex flex-col text-slate-600 dark:text-white py-4 h-[400px] pr-3">
+          <div className="w-full flex flex-col text-slate-600 dark:text-white py-4 h-[400px] pr-3 ">
             <Link
               to="#"
-              className={`flex gap-2 justify-start items-center pl-2 py-[5px]`}
+              className={`flex gap-2 justify-start items-center pl-2 py-[5px] active:pl-3`}
             >
               <div className="w-[30px] h-[30px] rounded-full relative">
                 <div className="w-[10px] h-[10px] rounded-full bg-green-500 absolute right-0 bottom-0"></div>
@@ -39,14 +67,14 @@ function Chat() {
               <span>ewewewe</span>
             </div>
             <div className="h-[400px] w-full bg-slate-100 dark:bg-[#131923]  p-3 rounded-md">
-              <div className="w-full h-full overflow-y-auto flex flex-col gap-3">
+              <div className="flex flex-col w-full h-full gap-3 overflow-y-auto">
                 <div className="w-full flex gap-2 justify-start items-center text-[14px]">
                   <img
                     className="w-[30px] h-[30px] rounded-full"
                     src="/images/user.png"
                     alt=""
                   />
-                  <div className="p-2 bg-purple-500 text-white rounded-md">
+                  <div className="p-2 text-white bg-purple-500 rounded-md">
                     <span>weewewewewewewe</span>
                   </div>
                 </div>
@@ -56,13 +84,13 @@ function Chat() {
                     src="/images/user.png"
                     alt=""
                   />
-                  <div className="p-2 bg-cyan-500 text-white rounded-md">
+                  <div className="p-2 text-white rounded-md bg-cyan-500">
                     <span>ewwwwwwwww</span>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="flex p-2 justify-between items-center w-full">
+            <div className="flex items-center justify-between w-full p-2">
               <div className="w-[40px] h-[40px] border p-2 justify-center items-center flex rounded-full dark:text-white">
                 <label className="cursor-pointer" htmlFor="">
                   <AiOutlinePlus />
@@ -73,9 +101,9 @@ function Chat() {
                 <input
                   type="text"
                   placeholder="input message"
-                  className="w-full rounded-full h-full outline-none p-3 dark:bg-transparent"
+                  className="w-full h-full p-3 rounded-full outline-none dark:bg-transparent"
                 />
-                <div className="text-2xl right-2 top-2 absolute cursor-auto">
+                <div className="absolute text-2xl cursor-auto right-2 top-2">
                   <span>
                     <GrEmoji />
                   </span>
@@ -89,7 +117,7 @@ function Chat() {
             </div>
           </div>{" "}
           :
-          <div className="w-full h-full flex justify-center items-center text-lg ont-bold text-slate-600">
+          <div className="flex items-center justify-center w-full h-full text-lg ont-bold text-slate-600">
             <span>select seller</span>
           </div>
         </div>
