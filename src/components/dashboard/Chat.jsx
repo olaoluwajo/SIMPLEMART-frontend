e3486e88480b91afe4182fc69921248d3036499a
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2024-10-11 00:27:06 +0100

    customer to seller feature ongoing

--- src/components/dashboard/Chat.jsx
+++ src/components/dashboard/Chat.jsx
@@ -1,36 +1,51 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { AiOutlineMessage, AiOutlinePlus } from "react-icons/ai";
 import { GrEmoji } from "react-icons/gr";
 import { IoSend } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
-import io from 'socket.io-client'
-import { add_friend } from "../../store/reducers/chatReducer";
+import io from "socket.io-client";
+import { add_friend, send_message } from "../../store/reducers/chatReducer";
 
 const socket = io("http://localhost:5000");
 
-
 function Chat() {
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  
   const { sellerId } = useParams();
   const { userInfo } = useSelector((state) => state.auth);
-  
+  const { fwb_messages, currentFd, my_friends } = useSelector(
+    (state) => state.chat
+  );
 
+  const [text, setText] = useState("");
 
-   useEffect(() => {
-     socket.emit("add_user", userInfo.id, userInfo);
-   }, [userInfo]);
+  useEffect(() => {
+    socket.emit("add_user", userInfo.id, userInfo);
+  }, [userInfo]);
 
-   useEffect(() => {
-     dispatch(
-       add_friend({
-         sellerId: sellerId || "",
-         userId: userInfo.id,
-       })
-     );
-   }, [dispatch, sellerId, userInfo]);
+  useEffect(() => {
+    dispatch(
+      add_friend({
+        sellerId: sellerId || "",
+        userId: userInfo.id,
+      })
+    );
+  }, [dispatch, sellerId, userInfo]);
+
+  function sendMessage() {
+    if (text) {
+      dispatch(
+        send_message({
+          userId: userInfo.id,
+          text,
+          sellerId,
+          name: userInfo.name,
+        })
+      );
+      setText("");
+    }
+  }
 
   return (
     <div className="bg-white dark:bg-[#232D3F]  p-3 rounded-md">
@@ -43,83 +58,116 @@ const dispatch = useDispatch();
             <span>Message</span>
           </div>
           <div className="w-full flex flex-col text-slate-600 dark:text-white py-4 h-[400px] pr-3 ">
-            <Link
-              to="#"
-              className={`flex gap-2 justify-start items-center pl-2 py-[5px] active:pl-3`}
-            >
-              <div className="w-[30px] h-[30px] rounded-full relative">
-                <div className="w-[10px] h-[10px] rounded-full bg-green-500 absolute right-0 bottom-0"></div>
+            {my_friends.map((f, i) => (
+              <Link
+                key={i}
+                to={`/dashboard/chat/${f.fdId}`}
+                className={`flex gap-2 justify-start items-center pl-2 py-[5px] hover:pl-3 transition-all duration-200`}
+              >
+                <div className=" size-[30px] rounded-full relative">
+                  <div className="w-[10px] h-[10px] rounded-full bg-green-500 absolute right-0 bottom-0"></div>
 
-                <img src="/images/user.png" alt="" className="rounded-full" />
-              </div>
-              <span>asdfsd</span>
-            </Link>
+                  <img
+                    src={f.image}
+                    alt={`${f.name} image`}
+                    className="rounded-full size-[30px] "
+                  />
+                </div>
+                <span>{f.name}</span>
+              </Link>
+            ))}
           </div>
         </div>
         <div className="w-[calc(100%-230px)]">
-          <div className="w-full h-full">
-            <div className="flex justify-start gap-3 items-center text-slate-600 dark:text-white text-xl h-[50px]">
-              <div className="w-[30px] h-[30px] rounded-full relative">
-                <div className="w-[10px] h-[10px] rounded-full bg-green-500 absolute right-0 bottom-0"></div>
+          {currentFd ? (
+            <div className="w-full h-full">
+              <div className="flex justify-start gap-3 items-center text-slate-600 dark:text-white text-md font-bold h-[50px]">
+                <div className="w-[30px] h-[30px] rounded-full relative">
+                  <div className="w-[10px] h-[10px] rounded-full bg-green-500 absolute right-0 bottom-0"></div>
 
-                <img src="/images/user.png" alt="" className="rounded-full" />
-              </div>
-              <span>ewewewe</span>
-            </div>
-            <div className="h-[400px] w-full bg-slate-100 dark:bg-[#131923]  p-3 rounded-md">
-              <div className="flex flex-col w-full h-full gap-3 overflow-y-auto">
-                <div className="w-full flex gap-2 justify-start items-center text-[14px]">
                   <img
-                    className="w-[30px] h-[30px] rounded-full"
-                    src="/images/user.png"
-                    alt=""
+                    src={currentFd.image}
+                    alt={`${currentFd.name} image`}
+                    className="rounded-full size-[30px]"
                   />
-                  <div className="p-2 text-white bg-purple-500 rounded-md">
-                    <span>weewewewewewewe</span>
+                </div>
+                <span>{currentFd.name}</span>
+              </div>
+              <div className="h-[400px] w-full bg-slate-100 dark:bg-[#131923]  p-3 rounded-md">
+                <div className="flex flex-col w-full h-full gap-3 overflow-y-auto">
+                  {fwb_messages.map((m, i) => {
+                    if (currentFd?.fdId !== m.receverId) {
+                      return (
+                        <div
+                          key={i}
+                          className="w-[90%] flex gap-2 justify-start items-end text-[14px]"
+                        >
+                          <img
+                            className="w-[30px] h-[30px] rounded-full"
+                            src="/images/user.png"
+                            alt=""
+                          />
+                          <div className="p-2 text-white bg-purple-500 rounded-md">
+                            <span>{m.message}</span>
+                          </div>
+                        </div>
+                      );
+                    } else {
+                      return (
+                        <div
+                          key={i}
+                          className=" w-[90%] flex gap-2 justify-end self-end items-end text-[14px]"
+                        >
+                          <div className=" p-2 text-white rounded-md bg-[#059473]">
+                            <span>{m.message}</span>
+                          </div>{" "}
+                          <img
+                            className="w-[30px] h-[30px] rounded-full"
+                            src="/images/user.png"
+                            alt=""
+                          />
+                        </div>
+                      );
+                    }
+                  })}
+                </div>
+              </div>
+              <div className="flex items-center justify-between w-full p-2">
+                <div className="w-[40px] h-[40px] border p-2 justify-center items-center flex rounded-full dark:text-white">
+                  <label className="cursor-pointer" htmlFor="">
+                    <AiOutlinePlus />
+                  </label>
+                  <input className="hidden" type="file" />
+                </div>
+                <div className="border h-[40px] p-0 ml-2 w-[calc(100%-90px)] rounded-full relative">
+                  <input
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    type="text"
+                    placeholder="input message"
+                    className="w-full h-full p-3 rounded-full outline-none dark:bg-transparent dark:text-white "
+                  />
+                  <div className="absolute text-2xl cursor-auto right-2 top-2 z-10 bg-white dark:bg-[#232D3F]  dark:text-white pl-3">
+                    <span>
+                      <GrEmoji />
+                    </span>
                   </div>
                 </div>
-                <div className="w-full flex gap-2 justify-end items-center text-[14px]">
-                  <img
-                    className="w-[30px] h-[30px] rounded-full"
-                    src="/images/user.png"
-                    alt=""
-                  />
-                  <div className="p-2 text-white rounded-md bg-cyan-500">
-                    <span>ewwwwwwwww</span>
+                <div
+                  onClick={sendMessage}
+                  className="w-[40px] p-2 justify-center items-center   cursor-pointer rounded-full active:bg-green-600 active:text-white"
+                >
+                  <div className="text-2xl dark:text-white">
+                    <IoSend />
                   </div>
                 </div>
               </div>
             </div>
-            <div className="flex items-center justify-between w-full p-2">
-              <div className="w-[40px] h-[40px] border p-2 justify-center items-center flex rounded-full dark:text-white">
-                <label className="cursor-pointer" htmlFor="">
-                  <AiOutlinePlus />
-                </label>
-                <input className="hidden" type="file" />
-              </div>
-              <div className="border h-[40px] p-0 ml-2 w-[calc(100%-90px)] rounded-full relative">
-                <input
-                  type="text"
-                  placeholder="input message"
-                  className="w-full h-full p-3 rounded-full outline-none dark:bg-transparent"
-                />
-                <div className="absolute text-2xl cursor-auto right-2 top-2">
-                  <span>
-                    <GrEmoji />
-                  </span>
-                </div>
-              </div>
-              <div className="w-[40px] p-2 justify-center items-center rounded-full">
-                <div className="text-2xl cursor-pointer dark:text-white">
-                  <IoSend />
-                </div>
-              </div>
+          ) : (
+            <div className="flex items-center justify-center w-full h-full text-lg ont-bold text-slate-600">
+              <span>select seller</span>
             </div>
-          </div>{" "}
-          :
-          <div className="flex items-center justify-center w-full h-full text-lg ont-bold text-slate-600">
-            <span>select seller</span>
-          </div>
+          )}
         </div>
       </div>
     </div>
--- src/store/reducers/chatReducer.js
+++ src/store/reducers/chatReducer.js
@@ -9,7 +9,24 @@ export const add_friend = createAsyncThunk(
         "/chat/customer/add-customer-friend",
         info
       );
-      console.log(data)
+      // console.log(data);
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+// End Method
+
+export const send_message = createAsyncThunk(
+  "chat/send_message",
+  async (info, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.post(
+        "/chat/customer/send-message-to-seller",
+        info
+      );
+      console.log(data);
       return fulfillWithValue(data);
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -22,7 +39,7 @@ export const chatReducer = createSlice({
   name: "chat",
   initialState: {
     my_friends: [],
-    fb_messages: [],
+    fwb_messages: [],
     currentFd: "",
     errorMessage: "",
     successMessage: "",
@@ -35,13 +52,27 @@ export const chatReducer = createSlice({
     },
   },
   extraReducers: (builder) => {
-    // builder
-    // .addCase(customer_register.fulfilled, (state, { payload }) => {
-    //     const userInfo = decodeToken(payload.token)
-    //     state.successMessage = payload.message;
-    //     state.loader = false;
-    //     state.userInfo = userInfo
-    // })
+    builder
+      .addCase(add_friend.fulfilled, (state, { payload }) => {
+        state.fwb_messages = payload.messages;
+        state.currentFd = payload.currentFd;
+        state.my_friends = payload.MyFriends;
+      })
+      .addCase(send_message.fulfilled, (state, { payload }) => {
+        let tempFriends = state.my_friends;
+        let index = tempFriends.findIndex(
+          (f) => f.fdId === payload.message.receverId
+        );
+        while (index > 0) {
+          let temp = tempFriends[index];
+          tempFriends[index] = tempFriends[index - 1];
+          tempFriends[index - 1] = temp;
+          index--;
+        }
+        state.my_friends = tempFriends;
+        state.fwb_messages = [...state.fwb_messages, payload.message];
+        state.successMessage = "Message Send Success";
+      });
   },
 });
 export const { messageClear } = chatReducer.actions;
